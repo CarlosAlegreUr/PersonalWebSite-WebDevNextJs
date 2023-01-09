f7f9b832a53d1aff97fb776fc1f29cd39abb0618
CarlosAlegre <calegreu@gmail.com>
2023-01-09 22:09:24 +0100

    Display error in ProjectsSection component fixed

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -37,22 +37,80 @@ export default function Projects({
 }
 
 function createGridFromProjectsData(projectsData: any) {
-  return (
-    <ul className="projects-list">
-      <li>
-        {projectsData.map((project: any, id: number) => {
-          return (
-            <ShowCaseBox
-              key={id}
-              title={project.name}
-              description={project.description}
-              image="/../public/images/my_face.jpg"
-              url={project.url}
-              imageAlt="One of developer's github projects, click to go github repository"
-            />
-          );
-        })}
-      </li>
-    </ul>
-  );
+  const images = mappingNameToImage(projectsData);
+  console.log(images);
+  const rowsNum = Math.floor(projectsData.length / 2) + 1;
+  const numOfRows = Array.from({ length: rowsNum }, () => 0);
+  const numOfCols = [1, 2];
+  let totalAdded = 0;
+
+  if (projectsData.length > 0) {
+    return (
+      <table>
+        <tbody>
+          {numOfRows.map((row: any, rowNum: number) => {
+            return (
+              <tr>
+                {numOfCols.map((col: any, numOfCol: number) => {
+                  if (totalAdded < projectsData.length) {
+                    const totalPositionId =
+                      numOfCol + rowNum * numOfCols.length;
+                    totalAdded += 1;
+                    return (
+                      <td>
+                        <ShowCaseBox
+                          key={totalPositionId}
+                          title={projectsData[totalPositionId].name}
+                          description={
+                            projectsData[totalPositionId].description
+                          }
+                          image={images[totalPositionId]}
+                          url={projectsData[totalPositionId].url}
+                          imageAlt="One of the developer's github projects, click to go github repository"
+                        />
+                      </td>
+                    );
+                  }
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  } else {
+    return <table></table>;
+  }
+}
+
+function mappingNameToImage(projects: any) {
+  const imagesArray = projects.map((project: any) => {
+    let image;
+    switch (project.name) {
+      case "BuddyFighters-FullstackWeb3NFTGame":
+        image = "/../public/images/projects-images/buddyFightersNFT.png";
+        break;
+      case "GaimonAndYou-FullstackClassicWebApp":
+        image = "/../public/images/projects-images/gaimon-and-you.png";
+        break;
+      case "DataStructures-Algorithms-DesignPatterns":
+        image =
+          "/../public/images/projects-images/data-structures-and-algo.jpg";
+        break;
+      case "InputControl-SmartContract-DesignPattern":
+        image = "/../public/images/projects-images/inputControl.png";
+        break;
+      case "PersonalWebSite-WebDevNextJs":
+        image = "/../public/images/my_face.jpg";
+        break;
+      case "FairTaxesAnalytics-WebDevReactJs":
+        image = "/../public/images/projects-images/FTA.jpg";
+        break;
+      default:
+        image = "/../public/images/default_image.jpg";
+        break;
+    }
+    return image;
+  });
+  return imagesArray;
 }
